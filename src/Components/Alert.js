49506de4dc6b0a3f1ea9de35a9c9e5f42e2272d0
Ishwari18 <ishwarirele18@.com>
2023-08-16 18:37:38 +0530

    idk

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -4,7 +4,7 @@ import "./css/Alert.css";
 export const Alert = () => {
   return (
     <>
-    <div className='alert'>You’ve got the freedom to learn what you want. <span className='discount'>50% discount</span> only until 17th August</div>
+    <div className='alert'> <span className='discount'>50% discount for Independence day</span> 15 - 17th August</div>
     </>
   )
 }
